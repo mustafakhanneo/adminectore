4dc03cc8d16c9cd055e25fcfd8361bd0474e2093
Mustafa Khan <mustafakhanneo@gmail.com>
2024-06-02 18:46:02 +0500

    hhg

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex w-full h-screen bg-white items-center justify-center">
-  <SignIn />
-  </div>
-  )
+      <SignIn />
+    </div>
+  );
 }
