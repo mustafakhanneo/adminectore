45aac7a608d1081c0ce41d1ea2d0a1b6dc9f7dd4
Mustafa Khan <mustafakhanneo@gmail.com>
2024-06-02 19:37:00 +0500

    hhf

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
 import {
   clerkMiddleware,
-  createRouteMatcher
 } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/*',
-]);
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
